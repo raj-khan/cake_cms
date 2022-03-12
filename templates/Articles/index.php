682909d483656a3e9d5a857cd28b16ffcad7ca23
raj_khan <meherullah97@gmail.com>
2022-03-13 05:20:30 +0600

    article view created

---
+++ templates/Articles/index.php
@@ -0,0 +1,18 @@
+<h1>Articles</h1>
+<table>
+    <tr>
+        <th>Title</th>
+        <th>Created</th>
+    </tr>
+
+    <!-- Here is where we iterate through our $articles query object, printing out article info -->
+
+    <?php foreach ($articles as $article): ?>
+        <td>
+            <?= $this->Html->link($article->title, ['action' => 'view', $article->slug]) ?>
+        </td>
+        <td>
+            <?= $article->created->format(DATE_RFC850) ?>
+        </td>
+    <?php endforeach; ?>
+</table>
