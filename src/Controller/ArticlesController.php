c747a634babd4251f664c319bafc52a7519011f4
raj_khan <meherullah97@gmail.com>
2022-03-13 05:52:28 +0600

    Save article module added

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -4,6 +4,15 @@ namespace App\Controller;
 
 class ArticlesController extends AppController
 {
+
+    public function initalize(): void 
+    {
+        parent::initialize();
+
+        $this->loadComponent('Paginator');
+        $this->loadComponent('Flash'); //Include the FlashComponent
+    }
+
     public function index()
     {
         $this->loadComponent('Paginator');
@@ -15,5 +24,26 @@ class ArticlesController extends AppController
     {
         $article = $this->Articles->findBySlug($slug)->firstOrFail();
         $this->set(compact('article'));
+    }
+
+    public function add()
+    {
+        $article = $this->Articles->newEmptyEntity();
+        if($this->request->is('post'))
+        {
+            $article = $this->Articles->patchEntity($article, $this->request->getData());
+
+            //Hardcoding the user_id is temporary, and will be removed later
+            // when we build authentication out.
+            $article->user_id = 1;
+
+            if($this->Articles->save($article))
+            {
+                $this->Flash->success(__('Your article has been saved'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to add your article'));
+        }
+        $this->set('article', $article);
     }
 }
